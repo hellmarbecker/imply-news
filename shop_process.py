eab3d6b0bd6ff91691a5f04779b10ffb1d0790ec
hellmarbecker <becker@hellmar-becker.de>
2020-09-06 17:45:41 +0200

    Add state machine mixin logic

--- shop_process.py
+++ shop_process.py
@@ -1,11 +1,14 @@
 import json
 import random
 import time
-from StateMachine import StateMachine, State
+from statemachine import StateMachine, State
+from statemachine.mixins import MachineMixin
+from statemachine.exceptions import TransitionNotAllowed
 
 baseurl = "https://imply-shop.com"
 
-class Session(StateMachine):
+
+class SessionMachine(StateMachine):
 
     landingPage = State('LandingPage', initial=True)
     shopPage = State('ShopPage')
@@ -20,19 +23,51 @@ class Session(StateMachine):
     toBasket = detailPage.to(addToBasket)
     toCheckout = addToBasket.to(checkoutPage)
     toPayment = checkoutPage.to(payment)
-    toExit = landingPage.to(exitSession) | shopPage.to(exitSession) | detailPage.to(exitSession) |
-        addToBasket.to(exitSession) | checkoutPage.to(exitSession) | payment.to(exitSession)
+    toExit = exitSession.from_(landingPage, shopPage, detailPage, addToBasket, checkoutPage)
+    advance = landingPage.to(shopPage) | shopPage.to(detailPage) | detailPage.to(addToBasket) \
+        | addToBasket.to(checkoutPage) | checkoutPage.to(payment) | payment.to(exitSession)
+
+
+class SessionModel(MachineMixin):
+    state_machine_name = 'SessionMachine'
+
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+        super(SessionModel, self).__init__()
+
+    def __repr__(self):
+        return "{}({!r})".format(type(self).__name__, self.__dict__)
+
 
 def main():
 
+    sessionId = 0
+    allSessions = []
+
     while True:
+        print('Top of loop')
+        print(f'Total elements in list: {len(allSessions)}')
         # With a certain probability, create a new session
-        if random.random() < 0.05:
-            newSession = Session()
+        if random.random() < 0.5:
+            sessionId += 1
+            print(f'--> Creating Session: id {sessionId}')
+            newSessionModel = SessionModel(state = 'landingPage', id = sessionId)
+            newSession = SessionMachine(newSessionModel)
             allSessions.append(newSession)
         # Pick one of the sessions
-        thisSession = random.choice(allSessions)
-        thisSession.advance()
+        try:
+            thisSession = random.choice(allSessions)
+            print(f'--> Session id {thisSession.model.id}')
+            thisSession.advance()
+            print(f'--> Session new state {thisSession.model.state}')
+        except IndexError:
+            print('--> No sessions to choose from')
+        except TransitionNotAllowed:
+            # Here we end up when the session was in exit state
+            print(f'--> removing session id {thisSession.model.id}')
+            allSessions.remove(thisSession)
+        time.sleep(1)
         
 
 if __name__ == "__main__":
