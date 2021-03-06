37cc58ad4730cf6470a51c37acc667c6e77defe5
hellmarbecker <becker@hellmar-becker.de>
2021-03-06 19:51:24 +0100

    Parse times

--- replay_file.pl
+++ replay_file.pl
@@ -4,14 +4,16 @@ use strict;
 use warnings;
 
 use Data::Dumper;
+use Date::Manip qw(ParseDate UnixDate);
 
 my $header = <>;
 chomp $header;
 my @fields = split(/,/, $header);
+for (@fields) { s/\"//g; }
 my %fieldPositions = map { $fields[$_] => $_ } (0 .. $#fields);
 
-print Dumper(\%fieldPositions);
-exit;
+print Dumper(\@fields);
+# print Dumper(\%fieldPositions);
 
 my %line;
 
@@ -20,5 +22,8 @@ while (<>) {
    my @data = split /,/;
    @line{@fields} = @data;
 
-   print Dumper(\%line);
+   my $ts = join(' ', @line{('DCTE', 'HRTE')});
+   my $date = ParseDate($ts);
+   print "$date\n";
+   #print Dumper(\%line);
 }
