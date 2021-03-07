04c9551377d998d7ec52bc37b19ed4482d108758
hellmarbecker <becker@hellmar-becker.de>
2021-03-07 11:34:30 +0100

    Add main loop

--- replay_file.pl
+++ replay_file.pl
@@ -3,10 +3,14 @@
 use strict;
 use warnings;
 
+use Fcntl qw(:seek);
 use Data::Dumper;
 use Date::Manip qw(ParseDate UnixDate);
+use JSON;
 
-my $header = <>;
+open INFILE, "<$ARGV[0]" or die "Could not open input file $ARGV[0]";
+
+my $header = <INFILE>;
 chomp $header;
 my @fields = split(/,/, $header);
 for (@fields) { s/\"//g; }
@@ -15,15 +19,28 @@ my %fieldPositions = map { $fields[$_] => $_ } (0 .. $#fields);
 print Dumper(\@fields);
 # print Dumper(\%fieldPositions);
 
-my %line;
+my $runDay = 0; # replay loop index
 
-while (<>) {
-   chomp;
-   my @data = split /,/;
-   @line{@fields} = @data;
+do {
+    print STDERR "loop index: $runDay\n";
 
-   my $ts = join(' ', @line{('DCTE', 'HRTE')});
-   my $date = ParseDate($ts);
-   print "$date\n";
-   #print Dumper(\%line);
-}
+    while (<INFILE>) {
+        chomp;
+        my @data = split /,/;
+        my %line;
+        @line{@fields} = @data;
+
+        my $ts = join(' ', @line{('DCTE', 'HRTE')});
+        my $date = ParseDate($ts);
+
+        my $json = encode_json \%line;
+        print "$date\n";
+        # print "$json\n";
+        #print Dumper(\%line);
+    }
+    $runDay++;
+    seek(INFILE, 0, SEEK_SET) or die "error: could not reset file handle\n";
+    $header = <INFILE>;
+
+} while (1);
+
