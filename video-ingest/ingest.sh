1680aea22dae2d4235201b096cbc8eda7e546e6e
hellmarbecker <becker@hellmar-becker.de>
2021-03-25 23:03:24 +0100

    Loop through dates

--- video-ingest/ingest.sh
+++ video-ingest/ingest.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-./gen-spec.pl --json-template ingest-user_view_video.json \
-  --access-key-id ... \
-  --secret-access-key ... \
-  --date 2019-06-11 \
-  --data-source user_view_video_1
+
+LINES=$(cat ./datelist)
+
+for DATE in $LINES; do
+
+  echo "Processing ingestion for $DATE"
+  ./gen-spec.pl --json-template ingest-user_view_video.json \
+    --access-key-id $AWS_ACCESS_KEY_ID \
+    --secret-access-key $AWS_SECRET_ACCESS_KEY \
+    --date $DATE \
+    --data-source user_view_video_1 >ingest-date.json
+  exit 
+
+done
 
