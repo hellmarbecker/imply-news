5bc2f91ad3f19bcc579e16ae7715384d020dd987
Hellmar Becker <becker@hellmar-becker.de>
2021-03-28 21:41:11 +0000

    Refactor

--- video-ingest/ingest-video_publication_history.sh
+++ video-ingest/ingest-video_publication_history.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-DATASOURCE=video_publication_history_raw
+DATASOURCE="video_publication_history_raw"
+ELB_URL="https://imply-b3e-elbexter-1ge3j2tooq0e9-1216598690.us-east-1.elb.amazonaws.com:9088"
 
 LINES=$(cat ./datelist-video_publication_history)
 
 for DATE in $LINES; do
 
-#  echo "\n----Processing ingestion for $DATE"
+#  echo "############ Processing ingestion for $DATE ############"
   ./gen-spec.pl --json-template ingest-video_publication_history.json \
     --access-key-id $AWS_ACCESS_KEY_ID \
     --secret-access-key $AWS_SECRET_ACCESS_KEY \
@@ -15,13 +16,13 @@ for DATE in $LINES; do
   TASK_ID=$(curl -s -u admin:UGaEl6qo7hx0zh5l526nPw== --cacert cert.pem \
     -X 'POST' -H "Content-Type: application/json" \
     --data @/tmp/ingest-$DATE.json \
-    https://imply-b3e-elbexter-n0nnt27t5gdw-723214491.us-east-1.elb.amazonaws.com:9088/druid/indexer/v1/task | jq .task | tr -d \" )
+    $ELB_URL/druid/indexer/v1/task | jq .task | tr -d \" )
   echo $TASK_ID
   while
     STATUS=$(curl -s -u admin:UGaEl6qo7hx0zh5l526nPw== --cacert cert.pem \
       -X 'GET' -H "Content-Type: application/json" \
       --data @/tmp/ingest-$DATE.json \
-      https://imply-b3e-elbexter-n0nnt27t5gdw-723214491.us-east-1.elb.amazonaws.com:9088/druid/indexer/v1/task/$TASK_ID/status)
+      $ELB_URL/druid/indexer/v1/task/$TASK_ID/status)
     echo $(date) $STATUS
     [[ $STATUS == *"HTTP"* || $STATUS == *"RUNNING"* ]]
   do
