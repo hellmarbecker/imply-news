ae66b8cff8ac48b1d450eed33e17ecd67ceba8b5
Hellmar Becker <becker@hellmar-becker.de>
2021-03-31 12:33:05 +0000

    Detail spec

--- video-ingest/ingest-user_view_video-d.sh
+++ video-ingest/ingest-user_view_video-d.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DATASOURCE=user_view_video_d
+DATASOURCE=user_view_video_raw
 
 LINES=$(cat ./datelist-user_view_video)
-ELB_URL="https://imply-b3e-elbexter-1ge3j2tooq0e9-1216598690.us-east-1.elb.amazonaws.com:9088"
+ELB_URL="https://imply-b18-elbexter-63dmtx8lxpcb-1363504918.us-east-1.elb.amazonaws.com:9088"
 
 for DATE in $LINES; do
 
@@ -13,21 +13,25 @@ for DATE in $LINES; do
     --secret-access-key $AWS_SECRET_ACCESS_KEY \
     --date $DATE \
     --data-source $DATASOURCE >/tmp/ingest-$DATE.json
-  TASK_ID=$(curl -s -u admin:UGaEl6qo7hx0zh5l526nPw== --cacert cert.pem \
+  TASK_ID=$(curl -s -u admin:XOJ68cdvVAqWWXVvLJKhUg== --cacert cert.pem \
     -X 'POST' -H "Content-Type: application/json" \
     --data @/tmp/ingest-$DATE.json \
     $ELB_URL/druid/indexer/v1/task | jq .task | tr -d \" )
-  echo $TASK_ID
-  while
-    STATUS=$(curl -s -u admin:UGaEl6qo7hx0zh5l526nPw== --cacert cert.pem \
-      -X 'GET' -H "Content-Type: application/json" \
-      --data @/tmp/ingest-$DATE.json \
-      $ELB_URL/druid/indexer/v1/task/$TASK_ID/status)
-    echo $(date) $STATUS
-    [[ $STATUS == *"HTTP"* || $STATUS == *"RUNNING"* ]]
-  do
-    sleep 5
-  done 
+  echo $DATE - $TASK_ID
+
+  sleep 10
+
+#  exit
+#  while
+#    STATUS=$(curl -s -u admin:XOJ68cdvVAqWWXVvLJKhUg== --cacert cert.pem \
+#      -X 'GET' -H "Content-Type: application/json" \
+#      --data @/tmp/ingest-$DATE.json \
+#      $ELB_URL/druid/indexer/v1/task/$TASK_ID/status)
+#    echo $(date) $STATUS
+#    [[ $STATUS == *"HTTP"* || $STATUS == *"RUNNING"* ]]
+#  do
+#    sleep 5
+#  done 
 
 done
 
