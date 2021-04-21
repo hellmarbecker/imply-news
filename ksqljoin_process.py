6ddfdc974202cadec72261311e5c869c85bb7fab
hellmarbecker <becker@hellmar-becker.de>
2021-04-21 16:13:36 +0200

    Add admin functions

--- ksqljoin_process.py
+++ ksqljoin_process.py
@@ -6,11 +6,37 @@ import datetime
 import argparse, sys, logging
 import socket
 from faker import Faker
-from confluent_kafka import Producer
+from confluent_kafka import Producer, AdminClient, NewTopic
 from exceptions import InvalidStateException, InvalidTransitionException
 
 key_field = 'uid'
 fake = Faker()
+
+# Admin - clean up topics, recreate topics
+
+def createTopics(conf):
+
+    topics = [config['General']['factTopic'], config['General']['dimTopic']]
+
+    # delete topics
+    a = AdminClient(conf['Kafka'])
+
+    a.delete_topics(topics, operation_timeout=10)
+
+    new_topics = [NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics]
+    # Note: In a multi-cluster production scenario, it is more typical to use a replication_factor of 3 for durability.
+
+    # Call create_topics to asynchronously create topics. A dict
+    # of <topic,future> is returned.
+    fs = a.create_topics(new_topics)
+
+    # Wait for each operation to finish.
+    for topic, f in fs.items():
+        try:
+            f.result()  # The result itself is None
+            print("Topic {} created".format(topic))
+        except Exception as e:
+            print("Failed to create topic {}: {}".format(topic, e))
 
 # Output functions - write to Kafka, or to stdout as JSON
 
