0cdcb6d4ebac9993055f52d74b8259d2dc6a26d5
hellmarbecker <becker@hellmar-becker.de>
2022-08-25 18:43:14 +0200

    Split and refactor

--- exceptions.py
+++
@@ -1,8 +0,0 @@
-class DataGeneratorError(Exception):
-    "Base exception for this project, all exceptions that can be raised inherit from this class."
-
-class InvalidStateException(DataGeneratorError):
-    "The current model state value is not mapped to a state definition."
-
-class InvalidTransitionException(DataGeneratorError):
-    "No legal transition."
