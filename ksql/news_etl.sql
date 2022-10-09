7efcd5122531d0bc82b78860a3ce5dad3f5af954
hellmarbecker <becker@hellmar-becker.de>
2022-10-09 17:17:52 +0200

    Add sessionize code

--- ksql/news_etl.sql
+++ ksql/news_etl.sql
@@ -79,3 +79,61 @@ SELECT
   `country_code`,
   `timezone`
 FROM `imply-news-cooked`;
+
+-- Sessionize in ksqlDB using a SESSION window. Because of the EMIT FINAL clause this has to be a TABLE.
+
+CREATE OR REPLACE TABLE `imply-news-sessions` WITH (
+  KAFKA_TOPIC='imply-news-sessions',
+  PARTITIONS=6,
+  KEY_FORMAT='KAFKA',
+  VALUE_FORMAT='JSON' ) AS
+SELECT
+  `sid`,
+  MIN(`timestamp`) AS session_start_time,
+  MAX(`timestamp`) AS session_end_time,
+  MAX(`useragent`) AS `useragent`,
+  COLLECT_LIST(`state`) AS `statesVisited`,
+  MAX(`uid`) AS `uid`,
+  MAX(`campaign`) AS `campaign`,
+  MAX(`channel`) AS `channel`,
+  MAX(`gender`) AS `gender`,
+  MAX(`age`) AS `age`,
+  MAX(`latitude`) AS `latitude`,
+  MAX(`longitude`) AS `longitude`,
+  MAX(`place_name`) AS `place_name`,
+  MAX(`country_code`) AS `country_code`,
+  MAX(`timezone`) AS `timezone`,
+  COUNT(*) AS session_depth
+FROM `imply-news-cooked`
+WINDOW SESSION (30 MINUTES)
+GROUP BY `sid`
+EMIT FINAL;
+
+-- Session changelog
+
+CREATE OR REPLACE TABLE `imply-news-sessions-changes` WITH (
+  KAFKA_TOPIC='imply-news-sessions-changes',
+  PARTITIONS=6,
+  KEY_FORMAT='KAFKA',
+  VALUE_FORMAT='JSON' ) AS
+SELECT
+  `sid`,
+  MIN(`timestamp`) AS session_start_time,
+  MAX(`timestamp`) AS session_end_time,
+  MAX(`useragent`) AS `useragent`,
+  COLLECT_LIST(`state`) AS `statesVisited`,
+  MAX(`uid`) AS `uid`,
+  MAX(`campaign`) AS `campaign`,
+  MAX(`channel`) AS `channel`,
+  MAX(`gender`) AS `gender`,
+  MAX(`age`) AS `age`,
+  MAX(`latitude`) AS `latitude`,
+  MAX(`longitude`) AS `longitude`,
+  MAX(`place_name`) AS `place_name`,
+  MAX(`country_code`) AS `country_code`,
+  MAX(`timezone`) AS `timezone`,
+  COUNT(*) AS session_depth
+FROM `imply-news-cooked`
+WINDOW SESSION (30 MINUTES)
+GROUP BY `sid`
+EMIT CHANGES;
