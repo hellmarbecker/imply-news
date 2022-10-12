f46f1ea8fbee8a2c0ed31fdf7e0b099e96130c29
Hellmar Becker <becker@hellmar-becker.de>
2022-10-12 21:51:10 +0200

    Update drop_news_etl.sql

--- ksql/drop_news_etl.sql
+++ ksql/drop_news_etl.sql
@@ -2,7 +2,7 @@ TERMINATE ALL; -- kill all persistent queries
 DROP TABLE IF EXISTS `imply-news-sessions-changes` DELETE TOPIC;
 DROP TABLE IF EXISTS `imply-news-sessions` DELETE TOPIC;
 DROP STREAM IF EXISTS `imply-news-avro` DELETE TOPIC;
-DROP STREAM IF EXISTS `imply-news-uk` DELETE TOPIC;
+DROP STREAM IF EXISTS `imply-news-de` DELETE TOPIC;
 DROP STREAM IF EXISTS `imply-news-cooked`; -- this shares a topic with imply-news-clicks which is dropped in the next step
 DROP STREAM IF EXISTS `imply-news-clicks` DELETE TOPIC;
 DROP STREAM IF EXISTS `imply-news-raw`; -- do not drop the input topic!!
